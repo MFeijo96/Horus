86dcf10ee91db5f6fe12b6d12b240d47d2f47444
Mauricio Feijo <mauricio.feijo@nl.com.br>
2019-09-04 22:45:03 -0300

    Click in carousel

--- HorusWEB/src/br/ucs/horus/ManagedBeans/MediaMB.java
+++ HorusWEB/src/br/ucs/horus/ManagedBeans/MediaMB.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Named;
 
 import br.ucs.horus.bean.MediaBean;
+import br.ucs.horus.models.ImageMedia;
 import br.ucs.horus.models.Media;
 import br.ucs.horus.models.Media.Type;
 import br.ucs.horus.models.Question;
+import br.ucs.horus.models.VideoMedia;
 
 @Named
 @SessionScoped
@@ -38,5 +42,9 @@ public class MediaMB implements Serializable {
 	
 	public String getMediaIcon(int type) {
 		return Type.getIcon(type);
+	}
+	
+	public void onMediaSelected(Media media){
+		System.out.println("mauricio batata" + media.getTitle()); 
 	}
 }
